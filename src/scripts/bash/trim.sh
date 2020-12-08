a0f7a4c6e80bf331744eab54e5f3501ef82a40f9
Kenneth Dait <49531569+kpdait@users.noreply.github.com>
2020-12-08 13:17:53 -0600

    bashrc: changed `trim' command to be an executable script

---
+++ src/scripts/bash/trim.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/env bash
+# trim.sh
+
+declare INPUT;
+if [[ -p /dev/stdin ]]; then
+    INPUT="$(cat /dev/stdin)";
+    [[ -z "${INPUT}" ]] && exit 11;
+else
+    if (($#>0)); then
+        INPUT="${*:?ERROR, no input detected}";
+    fi;
+fi;
+declare OUTPUT;
+OUTPUT="$(builtin echo "${INPUT}" \
+            | sed -E \
+                -e 's/^[[:blank:]]*//' \
+                -e 's/[[:blank:]]*$//'\
+         )" \
+    || exit 12;
+[[ -z "${OUTPUT:-}" ]] && exit 13;
+
+builtin echo -e "${OUTPUT}";
+
+exit
+
